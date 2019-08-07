242cde5ff898abe38edea3ccef0943917df85faa
Oka Nuantara <okanuantara1709@gmail.com>
2019-08-07 11:18:58 +0800

    sajlfgasuhdlkasndcjlsahcia

--- app/Http/Controllers/RekonsiliasiController.php
+++ app/Http/Controllers/RekonsiliasiController.php
@@ -121,9 +121,10 @@ class RekonsiliasiController extends Controller
                 foreach($transaksi as $item){
                     $total =  $item->total_pemasukan - $item->total_pengeluaran;                    
                     $data[$item->kategori]['hasil_transaksi'] = $total;
-                    $data[$item->kategori]['status'] = $total === $data[$item->kategori]['hasil_lpj'] ? 'SESUAI' : 'TIDAK SESUAI';
+                    @$data[$item->kategori]['status'] = $total === $data[$item->kategori]['hasil_lpj'] ? 'SESUAI' : 'TIDAK SESUAI';
                 }
             }
+            // dd($data);
             
         }
         $kategori = $this->kategori;
